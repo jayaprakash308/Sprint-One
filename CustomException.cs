4cef9a872e74af5c1fb80f227b53c9ea730fa43f
jayaprakash308 <99663001+jayaprakash308@users.noreply.github.com>
2022-02-17 09:50:27 +0530

    Add files via upload

---
+++ CustomException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FMS.Exceptions 
+{
+    public class FacultyExceptions : Exception
+    {
+        public FacultyExceptions() : base()
+        {
+           
+        }
+
+        public FacultyExceptions(string message) : base(message)
+        {
+            
+        }
+        public FacultyExceptions(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+
+    }
+}
